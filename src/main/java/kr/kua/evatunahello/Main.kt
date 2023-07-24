bc68a2a6e2428c698576db57b85607f07d2e7724
HoYoung <kgerust@gmail.com>
2023-07-24 14:08:09 +0900

    Feat: Update

--- src/main/java/kr/kua/evatunahello/Main.kt
+++ src/main/java/kr/kua/evatunahello/Main.kt
@@ -1,14 +1,23 @@
 package kr.kua.evatunahello
 
+import com.google.common.io.ByteStreams
 import kr.kua.evatunahello.command.SearchCommand
 import kr.kua.evatunahello.listener.EventListenerBase
+import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.ProxyServer
+import net.md_5.bungee.api.chat.TextComponent
+import net.md_5.bungee.api.connection.ProxiedPlayer
 import net.md_5.bungee.api.plugin.Plugin
+import org.bukkit.entity.Player
+import org.bukkit.plugin.messaging.PluginMessageListener
 
-class Main : Plugin() {
+
+class Main : Plugin(), PluginMessageListener {
     override fun onEnable() {
         val pm = proxy.pluginManager
         pm.registerListener(this, EventListenerBase(this))
+
+        ProxyServer.getInstance().registerChannel("NewbieHello")
 
         ProxyServer.getInstance().pluginManager.registerCommand(this, SearchCommand())
 
@@ -23,6 +32,24 @@ class Main : Plugin() {
         return when (name) {
             "Geralt_w", "LopLove", "dohun1", "1L_Bottle", "creamcrepe", "AppleCrepe" -> true
             else -> false
+        }
+    }
+
+    override fun onPluginMessageReceived(channel: String, player: Player, message: ByteArray?) {
+        if (channel != "NewbieHello") return
+
+        val username = ByteStreams.newDataInput(message)
+        broadcastMessage(
+            ProxyServer.getInstance().players,
+            "\uD83C\uDF89 " + ChatColor.of("#FF55FF") + username + " §7님이 서버에 처음 접속 하셨습니다. §r\uD83D\uDC4F\uD83D\uDC4F"
+        )
+    }
+
+    private fun broadcastMessage(players: Iterable<ProxiedPlayer>, content: String) {
+        val message = TextComponent.fromLegacyText(content)
+        for (player: ProxiedPlayer in players) {
+            if (player.server != null && player.server.info.name == "guest") return
+            player.sendMessage(*message)
         }
     }
 }
--- src/main/java/kr/kua/evatunahello/listener/EventListenerBase.kt
+++ src/main/java/kr/kua/evatunahello/listener/EventListenerBase.kt
@@ -11,6 +11,7 @@ import net.md_5.bungee.api.event.ServerConnectEvent
 import net.md_5.bungee.api.event.ServerSwitchEvent
 import net.md_5.bungee.event.EventPriority
 import net.md_5.bungee.api.plugin.Listener
+import net.md_5.bungee.event.EventHandler
 
 class EventListenerBase(plugin: Main) : Listener {
 
@@ -20,12 +21,12 @@ class EventListenerBase(plugin: Main) : Listener {
         this.plugin = plugin
     }
 
-    @net.md_5.bungee.event.EventHandler(priority = EventPriority.LOW)
+    @EventHandler(priority = EventPriority.LOW)
     fun ServerConnectEvent(event: ServerConnectEvent) {
         val player = event.player
         try {
 //            if (plugin.isBot(player.name)) return
-            if (event.target.name == "guest") return
+//            if (event.target.name == "guest") return
             if (event.reason != ServerConnectEvent.Reason.JOIN_PROXY) return
         } catch (ex: Exception) {
         }
@@ -33,42 +34,45 @@ class EventListenerBase(plugin: Main) : Listener {
         if (!isBanned) sendJoinMessage(ProxyServer.getInstance().players, player.name)
     }
 
-    @net.md_5.bungee.event.EventHandler(priority = EventPriority.NORMAL)
+    @EventHandler(priority = EventPriority.NORMAL)
     fun PlayerDisconnectEvent(event: PlayerDisconnectEvent) {
         val player = event.player
         try {
 //            if (plugin.isBot(player.name)) return
-            if (event.player.server.info.name == "guest") return
+//            if (event.player.server.info.name == "guest") return
             sendLeaveMessage(ProxyServer.getInstance().players, player.name)
         } catch (ex: Exception) {
         }
     }
 
-    @net.md_5.bungee.event.EventHandler(priority = EventPriority.NORMAL)
-    fun ServerSwitchEvent(event: ServerSwitchEvent) {
-        val player = event.player
-        try {
-//            if (plugin.isBot(player.name)) return
-            if (event.from == null) return
-            if (event.from.name != "guest") return
-        } catch (ex: Exception) {
-        }
-        broadcastMessage(
-            ProxyServer.getInstance().players,
-            "\uD83C\uDF89 " + ChatColor.of("#FF55FF") + player.name + " §7님이 서버에 처음 접속 하셨습니다. §r\uD83D\uDC4F\uD83D\uDC4F"
-        )
-//        broadcastMessage(ProxyServer.getInstance().getPlayers(),
-//                "\ue4db " + ChatColor.of("#FF55FF") + player.getName() + "님이 서버에 처음 접속 하셨습니다. :)");
-//        );
-    }
+    /**
+     * guest 서버 제거로 인한 이벤트 제거
+     */
+//    @EventHandler(priority = EventPriority.NORMAL)
+//    fun ServerSwitchEvent(event: ServerSwitchEvent) {
+//        val player = event.player
+//        try {
+////            if (plugin.isBot(player.name)) return
+//            if (event.from == null) return
+//            if (event.from.name != "guest") return
+//        } catch (ex: Exception) {
+//        }
+//        broadcastMessage(
+//            ProxyServer.getInstance().players,
+//            "\uD83C\uDF89 " + ChatColor.of("#FF55FF") + player.name + " §7님이 서버에 처음 접속 하셨습니다. §r\uD83D\uDC4F\uD83D\uDC4F"
+//        )
+////        broadcastMessage(ProxyServer.getInstance().getPlayers(),
+////                "\ue4db " + ChatColor.of("#FF55FF") + player.getName() + "님이 서버에 처음 접속 하셨습니다. :)");
+////        );
+//    }
 
-    private fun broadcastMessage(players: Iterable<ProxiedPlayer>, content: String) {
-        val message = TextComponent.fromLegacyText(content)
-        for (player: ProxiedPlayer in players) {
-            if (player.server != null && player.server.info.name == "guest") return
-            player.sendMessage(*message)
-        }
-    }
+//    private fun broadcastMessage(players: Iterable<ProxiedPlayer>, content: String) {
+//        val message = TextComponent.fromLegacyText(content)
+//        for (player: ProxiedPlayer in players) {
+//            if (player.server != null && player.server.info.name == "guest") return
+//            player.sendMessage(*message)
+//        }
+//    }
 
     private fun sendJoinMessage(players: Iterable<ProxiedPlayer>, name: String) {
         val message = TextComponent.fromLegacyText(
